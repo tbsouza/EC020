dfbbcc27860071f55ca6951386d20aacc49c5dc2
tbsouza <tbsouza@outlook.com>
2017-04-10 09:24:24 -0300

    Versão Apresentada
    Versão apresentada na aula de 10/04.
    Sistema de controle de irrigação em plantações.

---
+++ bnc_oled/Debug/src/main.d
@@ -0,0 +1,46 @@
+src/main.d: ../src/main.c \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_pinsel.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc_types.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_gpio.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_i2c.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_ssp.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_adc.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_timer.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_uart.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/oled.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/temp.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/uart2.h \
+ C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/led7seg.h
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_pinsel.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc_types.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_gpio.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_i2c.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_ssp.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_adc.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_timer.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_MCU\inc/lpc17xx_uart.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/oled.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/temp.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/uart2.h:
+
+C:\Users\tbsou\Documents\LPCXpresso_6.1.4_194\workspace\Lib_EaBaseBoard\inc/led7seg.h:
--- bnc_oled/src/main.c
+++ bnc_oled/src/main.c
@@ -7,17 +7,61 @@
  *
  ******************************************************************************/
 
+// *************************** INCLUDES ***************************
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_gpio.h"
 #include "lpc17xx_i2c.h"
 #include "lpc17xx_ssp.h"
 #include "lpc17xx_adc.h"
 #include "lpc17xx_timer.h"
+#include "lpc17xx_uart.h"
 
 #include "oled.h"
+#include "temp.h"
+#include "uart2.h"
+#include "led7seg.h"
+// *****************************************************************
+
+
+// ******** Definições *************
+#define UART_DEV LPC_UART3
+//**********************************************
+
 
 static uint8_t buf[10];
 
+// ************ Usado na temperatura ************
+uint32_t getMsTicks(void);
+static uint32_t msTicks = 0;
+//**********************************************
+
+// ************* Inicializa UART *************
+static void init_uart(void)
+{
+	PINSEL_CFG_Type PinCfg;
+	UART_CFG_Type uartCfg;
+
+	/* Initialize UART3 pin connect */
+	PinCfg.Funcnum = 2;
+	PinCfg.Pinnum = 0;
+	PinCfg.Portnum = 0;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 1;
+	PINSEL_ConfigPin(&PinCfg);
+
+	uartCfg.Baud_rate = 115200;
+	uartCfg.Databits = UART_DATABIT_8;
+	uartCfg.Parity = UART_PARITY_NONE;
+	uartCfg.Stopbits = UART_STOPBIT_1;
+
+	UART_Init(UART_DEV, &uartCfg);
+
+	UART_TxCmd(UART_DEV, ENABLE);
+}
+//**********************************************
+
+
+//*************************************************************************************
 static void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
 {
     static const char* pAscii = "0123456789abcdefghijklmnopqrstuvwxyz";
@@ -69,7 +113,10 @@ static void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
 
     return;
 }
+//*************************************************************************************
 
+
+//*************************** INITs ***********************************
 static void init_ssp(void)
 {
 	SSP_CFG_Type SSP_ConfigStruct;
@@ -104,9 +151,7 @@ static void init_ssp(void)
 
 	// Enable SSP peripheral
 	SSP_Cmd(LPC_SSP1, ENABLE);
-
 }
-
 
 static void init_i2c(void)
 {
@@ -151,21 +196,48 @@ static void init_adc(void)
 	ADC_ChannelCmd(LPC_ADC,ADC_CHANNEL_5,ENABLE);
 
 }
+// *************************************************************************
 
 
+// *************************** MAIN ***************************
 int main (void) {
 
     uint32_t val  = 0;
 
+    // Variaveis UART
+    uint8_t uart1Read = 1;
+    uint32_t recvd = 0;
+    uint32_t len = 0;
+    uint8_t data = 0;
+
+    // Modos:
+    //       1 - vaalvula automatica (inicial)
+    //		 2 - Abre manualmente
+    // 		 3 - Fecha manualmente
+    //		 4 - Mostra temperatura
+    uint8_t mode = 1;
+
+    // Inicialização
     init_i2c();
     init_ssp();
     init_adc();
-
     oled_init();
+    init_uart();
+    uart2_init(115200, CHANNEL_A);
+    led7seg_init();
 
-    oled_clearScreen(OLED_COLOR_WHITE);
+    // Init sensor temperatura
+    SysTick_Config(SystemCoreClock / 1000);
+    temp_init (&getMsTicks);
 
-    oled_putString(1,1,  (uint8_t*)"BNC: ", OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+    // Deixa oled fundo preto
+    oled_clearScreen(OLED_COLOR_BLACK);
+
+    // Escreve no oled
+    oled_putString(1,1,  "Temp: ", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+    // Seta led 7 segmentos para 1 inicialmente
+    led7seg_setChar( '1', 0 );
 
     while(1) {
 
@@ -175,19 +247,88 @@ int main (void) {
     	while (!(ADC_ChannelGetStatus(LPC_ADC,ADC_CHANNEL_5,ADC_DATA_DONE)));
     	val = ADC_ChannelGetData(LPC_ADC,ADC_CHANNEL_5);
 
-        /* output values to OLED display */
 
-        intToString(val, buf, 10, 10);
-        oled_fillRect((1+6*6),1, 80, 8, OLED_COLOR_WHITE);
-        oled_putString((1+6*6),1, buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+    	// Le temperatura e passa para celsius
+    	int32_t celsius = temp_read()/10;
 
+    	// Converte int32_t para char* para exibir no oled
+    	uint8_t *c[3];
+    	sprintf(c, "%d", celsius);
+
+        //oled_fillRect((1+6*6),1, 80, 8, OLED_COLOR_BLACK); // Desenha retangulo no oled
+        oled_putString((1+6*6),1, c, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+        // Leitura do UART
+        len = UART_Receive(UART_DEV, &data, 1, NONE_BLOCKING);
+
+        // Mudança dos modos
+        // len>0 significa alguma informação chegando no uart
+        if( (len>0) && (data)=='1' ){ mode = 1; }
+
+        if( (len>0) && (data)=='2' ){ mode = 2; }
+
+        if( (len>0) && (data)=='3' ){ mode = 3; }
+
+        if( (len>0) && (data)=='4' ){ mode = 4; }
+
+        // Ações de cada modo
+
+
+        if( mode == 1 ){ // Modo 1
+
+        	// Led & segmentos valor 1
+        	led7seg_setChar( '1', 0 );
+
+			if( celsius  >= 25 ){
+				oled_putString(1,12,  "Valvula  aberta", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+			}else if( celsius  < 22 ){
+				oled_putString(1,12,  "Valvula fechada", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+			}
+        }
+
+
+        else if( mode == 2 ){ // Modo 2
+
+        	led7seg_setChar( '2', 0 );
+        	oled_putString(1,12,  "Valvula  aberta", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+        }else if( mode == 3 ){ // Modo 3
+
+        	led7seg_setChar( 'E', 0 );
+        	oled_putString(1,12,  "Valvula fechada", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
+
+        }else if( mode == 4 ){ // Modo 4
+
+        	led7seg_setChar( '-', 0 );
+
+        	UART_SendString(UART_DEV, (uint8_t*)c );	  // mostra temp no uart
+        	UART_SendString(UART_DEV, (uint8_t*)"\r\n" ); // Pula linha
+
+        	Timer0_Wait(350);
+
+        	// Mostra a temperatura no uart e volta para modo 1
+        	mode = 1;
+        }
 
         /* delay */
-        Timer0_Wait(200);
+        Timer0_Wait(1000);
     }
+}
+// *********************************************************************************
 
+
+// *************************** Temperatura ***************************
+void SysTick_Handler(void) {
+    msTicks++;
 }
 
+uint32_t getMsTicks(void)
+{
+    return msTicks;
+}
+// *******************************************************************
+
+// *******************************************************************
 void check_failed(uint8_t *file, uint32_t line)
 {
 	/* User can add his own implementation to report the file name and line number,
@@ -196,3 +337,5 @@ void check_failed(uint8_t *file, uint32_t line)
 	/* Infinite loop */
 	while(1);
 }
+// *******************************************************************
+
